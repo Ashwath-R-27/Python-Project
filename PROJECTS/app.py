2f1f1f361e75f2b90cd21d031c25379ff74cb9a3
Ashwath-R-27 <ashwathr27100@gmail.com>
2025-11-25 15:53:43 +0530

    Add files via upload

--- PROJECTS/app.py
+++ PROJECTS/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def index():
     return render_template('adminlogin.html')
 
-@app.route('/verify', methods=['POST'])
+@app.route('/dashboard', methods=['POST'])
 def logverify():
     un = str(request.form['un'])
     pwd = request.form['pwd']
@@ -19,5 +19,20 @@ def logverify():
     else:
         result='username'
         return render_template('adminlogin.html',result=result)
+
+records=[[101,'Veg Biriyani',10,100],
+         [102,'Chilly chicken',12,165],
+         [103,'Idly',25,30],
+         [104,'Dosa',30,25],
+         [105,'Chappathi',25,15]]
+expense=[]
+for i in records:
+    i.append(i[2]*i[3])
+    expense.append(i)
+
+@app.route('/dashboard/accounts')
+def accounts():
+    return render_template('accounts.html',records=expense)
+
 if __name__ == '__main__':
     app.run(debug=True)
